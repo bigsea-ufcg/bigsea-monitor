9904af5c8fbebd3b6b27e78bd0e0eb59eb26fb8f
armstrongmsg <armstrongmsg@gmail.com>
2017-05-10 14:51:21 -0300

    setup.sh is now non-interactive.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt-get update && apt-get install build-essential libffi-dev python-pip python-dev libssl-dev
+sudo apt-get update && apt-get install -y build-essential libffi-dev python-pip python-dev libssl-dev
 sudo pip install pytz
 sudo pip install -r requirements.txt
