da895e74deb07e97f926c32bed130beb92d48f29
Roberto Nascimento Jr <robertonscjr@gmail.com>
2018-05-29 17:08:09 -0300

    Update openstack generic parameters

--- monitor/plugins/openstack_generic/plugin.py
+++ monitor/plugins/openstack_generic/plugin.py
@@ -21,64 +21,77 @@ from monitor.plugins.base import Plugin
 
 
 class OSGeneric(Plugin):
-
     def __init__(self, app_id, info_plugin, keypair, retries=60):
-        Plugin.__init__(self, app_id, info_plugin, collect_period=5, retries=retries)
+        Plugin.__init__(self, app_id, info_plugin, collect_period=5,
+                        retries=retries)
+
+        self.app_id = app_id
         self.host_ip = info_plugin['host_ip']
         self.expected_time = info_plugin['expected_time']
+        self.log_path = info_plugin['log_path']
         self.keypair_path = keypair
         self.host_username = 'ubuntu'
-        self.log_path = info_plugin['log_path']
         self.dimensions = {"application_id": self.app_id,
                            "host": self.host_ip}
+
         self.last_checked = ''
         self.start_time = time.time()
         self.monasca = MonascaConnector()
 
-    # This method extracts the value information from a log line
-    # that contains the measurement for the interest metric
+    """ This method extracts the value information from a log line
+        that contains the measurement for the interest metric """
     def _get_metric_value(self, log):
         value = None
         for i in range(len(log) - 1, 0, -1):
             if log[i] == '#':
                 value = float(log[i + 1:-1])
+
         return value
 
     def _get_elapsed_time(self):
         delay = time.time() - self.start_time
         return delay
 
-    # This method returns a remote connection with the host where
-    # the log will be captured. It is possible to execute a command
-    # in the host using the function c.exec_command("write_command_here")
-    # with the object returned here
+    """ This method returns a remote connection with the host where
+        the log will be captured. It is possible to execute a command
+        in the host using the function c.exec_command("write_command_here")
+        with the object returned here """
     def _get_ssh_connection(self):
         keypair = paramiko.RSAKey.from_private_key_file(self.keypair_path)
         conn = paramiko.SSHClient()
         conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        conn.connect(hostname=self.host_ip, username=self.host_username, pkey=keypair)
+        conn.connect(hostname=self.host_ip, username=self.host_username,
+                     pkey=keypair)
+
         return conn
 
-    # This is an auxiliary function to prepare and publish the metric. The point is to keep
-    # monitoring_application as simple as possible.
+    """ This is an auxiliary function to prepare and publish the metric.
+        The point is to keep monitoring_application as simple as possible. """
     def _publish_metrics(self, last_log):
         metric = {}
         print last_log
+
         # Check if this log line contains a new metric measurement.
         if '[Progress]' in last_log and self.last_checked != last_log:
             self.last_checked = last_log
-            # Add to metric_info values for this measurement:
-            # value and timestamp
+
+            """ Add to metric_info values for this measurement:
+                value and timestamp """
             ref_value = self._get_elapsed_time() / self.expected_time
             measurement_value = self._get_metric_value(last_log)
             error = measurement_value - ref_value
-            # The Monasca metric must have the 3 following fields to be created: name, value and timestamp,
-            # but also is possible to increment the metrics identities - using dimensions -  and informations -
-            # using value_meta, a dictionary that contains aditional information about the measurement  .
+
+            """ The Monasca metric must have the 3 following fields to be
+                created: name, value and timestamp, but also is possible to
+                increment the metrics identities - using dimensions -  and
+                informations - using value_meta, a dictionary that contains
+                aditional information about the measurement. """
+
             metric['name'] = 'application-progress.error'
             metric['value'] = error
             metric['timestamp'] = time.time() * 1000
             metric['dimensions'] = self.dimensions
+
             # Sending the metric to Monasca
             self.monasca.send_metrics([metric])
             print "Application progress error: %.4f" % error
@@ -91,13 +104,20 @@ class OSGeneric(Plugin):
         try:
             # First of all, a connection with the host is created.
             conn = self._get_ssh_connection()
-            # The second step consists in execute the command to capture the last log line from the log
-            # file using the connection create below and saving the outputs.
-            stdin , stdout, stderr = conn.exec_command("sudo tail -1 %s" % self.log_path)
-            # The last step is to actually publish using the captured log line.
+
+            """ The second step consists in execute the command to capture
+                the last log line from the log file using the connection
+                create below and saving the outputs. """
+            stdin , stdout, stderr = conn.exec_command(
+                                         "sudo tail -1 %s" % self.log_path)
+
+            # The last step is to actually publish using the captured log line
             self._publish_metrics(stdout.read())
 
         except Exception as ex:
-            print "Monitoring %s is not possible. \nError: %s. %s remaining attempts" % (self.app_id, ex.message,
-                                                                                         self.attempts)
+            print "Monitoring %s is not possible. \n\
+                   Error: %s. %s remaining attempts" % (self.app_id,
+                                                        ex.message,
+                                                        self.attempts)
+
             raise ex
