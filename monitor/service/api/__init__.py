ec003ad06b5138cc208789a224e7c3ab36ad5732
Roberto Nascimento Jr <robertonscjr@gmail.com>
2018-06-20 16:44:10 -0300

    Add validation of plugins parameters

--- monitor/service/api/__init__.py
+++ monitor/service/api/__init__.py
@@ -18,29 +18,39 @@ import os
 import sys
 
 
-# Conf reading
-config = ConfigParser.RawConfigParser()
-config.read('./monitor.cfg')
+try:
+    # Conf reading
+    config = ConfigParser.RawConfigParser()
+    config.read('./monitor.cfg')
+    
+    """ General configuration """
+    address = config.get('general', 'host')
+    port = config.getint('general', 'port')
+    plugins = config.get('general', 'plugins').split(',')
+    use_debug = config.get('general', 'debug')
 
-""" General configuration """
-address = config.get('general', 'host')
-port = config.getint('general', 'port')
-plugins = config.get('general', 'plugins').split(',')
-use_debug = config.get('general', 'debug')
+    """ Validate if really exists a section to listed plugins """
+    for plugin in plugins:
+        if plugin not in config.sections():
+            raise Exception("plugin '%s' section missing" % plugin)
+    
+    if 'openstack_generic' in plugins:
+        retries = config.getint('openstack_generic', 'retries')
+        os_keypair = config.get('openstack_generic', 'key_pair')
+    
+    if 'spark_mesos' in plugins:
+        mesos_cluster_addr = config.get('spark_mesos', 'mesos_cluster_addr')
+        mesos_password = config.get('spark_mesos', 'mesos_password')
+        mesos_username = config.get('spark_mesos', 'mesos_username')
+    
+    """ Monasca parameters """
+    monasca_endpoint = config.get('monasca', 'monasca_endpoint')
+    monasca_username = config.get('monasca', 'username')
+    monasca_password = config.get('monasca', 'password')
+    monasca_auth_url = config.get('monasca', 'auth_url')
+    monasca_project_name = config.get('monasca', 'project_name')
+    monasca_api_version = config.get('monasca', 'api_version')
 
-if 'openstack_generic' in plugins:
-    retries = config.getint('openstack_generic', 'retries')
-    os_keypair = config.get('openstack_generic', 'key_pair')
-
-if 'spark_mesos' in plugins:
-    mesos_cluster_addr = config.get('spark_mesos', 'mesos_cluster_addr')
-    mesos_password = config.get('spark_mesos', 'mesos_password')
-    mesos_username = config.get('spark_mesos', 'mesos_username')
-
-""" Monasca parameters """
-monasca_endpoint = config.get('monasca', 'monasca_endpoint')
-monasca_username = config.get('monasca', 'username')
-monasca_password = config.get('monasca', 'password')
-monasca_auth_url = config.get('monasca', 'auth_url')
-monasca_project_name = config.get('monasca', 'project_name')
-monasca_api_version = config.get('monasca', 'api_version')
+except Exception as e:
+    print "Error: %s" % e.message
+    quit()
